502e395bcd4da00f14f4a9fb41517eb29fc9be37
Umer Arshad <umerm64@gmail.com>
2021-07-07 14:20:32 +0500

    adding buttons to control color

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,31 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_btnAstroid_clicked()
+{
+    this->ui->renderArea->setBackgroundColor(Qt::red);
+    this->ui->renderArea->repaint();
+}
+
+
+void MainWindow::on_btnCicloid_clicked()
+{
+    this->ui->renderArea->setBackgroundColor(Qt::green);
+    this->ui->renderArea->repaint();
+}
+
+
+void MainWindow::on_btnHuygens_clicked()
+{
+    this->ui->renderArea->setBackgroundColor(Qt::blue);
+    this->ui->renderArea->repaint();
+}
+
+
+void MainWindow::on_btnHypo_clicked()
+{
+    this->ui->renderArea->setBackgroundColor(Qt::yellow);
+    this->ui->renderArea->repaint();
+}
+
--- renderarea.h
+++ renderarea.h
@@ -13,6 +13,11 @@ public:
     QSize minimumSizeHint() const Q_DECL_OVERRIDE;
     QSize sizeHint() const Q_DECL_OVERRIDE;
 
+    enum Shapes { Astroid, Cicloid, HuygensCicloid, HypoCicloid };
+
+    void setBackgroundColor (QColor color) { mBackgroundColor = color; } //setter
+    QColor getBackgroundColor () const { return mBackgroundColor; } //getter
+
 protected:
     void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
 
